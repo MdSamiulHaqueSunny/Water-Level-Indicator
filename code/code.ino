06c1018c409219bae2483e81806ebb2cac58b2f4
Md Samiul Haque Sunny <mdshs31@gmail.com>
2016-05-13 00:39:15 +0600

    Add files via upload

---
+++ code/code.ino
@@ -0,0 +1,42 @@
+#include <LiquidCrystal.h>
+int ir[] = {A0, A1, A2};
+int reading[] = {0, 0, 0};
+
+LiquidCrystal lcd(4, 5,6,7,8,9);
+#define LCD_CLEAR 0b00000001
+
+void setup(void){
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  for (int i = 0; i < 3; i++){
+    pinMode(ir[i], INPUT);
+  }
+}
+
+void read(void){
+  for (int i = 0; i < 3; i++){
+    reading[i] = analogRead(ir[i])*30;
+  }
+}
+
+void print(void){
+  Serial.println(" ===== BEGIN ======== \n\n");
+  read();
+  Serial.println("LOW: " + String(reading[0]));
+  Serial.println("MID: " + String(reading[1]));
+  Serial.println("HIGH: " + String(reading[2]));
+  Serial.println();
+  Serial.println("======= END ======== \n\n");
+  delay(1000);
+}
+void checkwaterlevel()
+{
+  
+  }
+
+void loop(void){
+  print();
+  lcd.setCursor(0, 0);
+lcd.print("WATER LEVEL:");
+
+}
---
+++ water_level_indicator/water_level_indicator/water_level_indicator.ino
@@ -0,0 +1,57 @@
+#include "NewPing.h"
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+
+#define MAX_DISTANCE 400
+
+LiquidCrystal lcd(4, 5,6,7,8,9);
+#define LCD_CLEAR 0b00000001
+
+
+
+void setup()
+{
+    Serial.begin(9600);
+    lcd.begin(16,2);
+   Serial.println("Serial initialized");
+    delay(3000);
+}
+
+
+int Distance;
+
+NewPing Sonar(13, 12, MAX_DISTANCE);
+
+
+void updateDistance()
+{
+   Serial.println("Updating distances: ");
+    Distance = Sonar.ping() / US_ROUNDTRIP_CM;
+    
+}
+void printdistance()
+{
+ Serial.println("Front: " + String(Distance) + " cm");
+delay(500);
+  }
+
+void loop()
+{
+  updateDistance();
+  printdistance();
+  lcd.setCursor(0, 0);
+lcd.print("WATER LEVEL:"+String(Distance)+" " +"CM");
+if(Distance<3)
+{
+ lcd.setCursor(0, 1);
+lcd.print("FULL"); 
+  }
+  else if(Distance>3&&Distance<8)
+  {lcd.setCursor(0, 1);
+lcd.print("MIDDLE");
+    }
+  else if(Distance>8)
+  {lcd.setCursor(0, 1);
+lcd.print("LOW");
+    }
+}
